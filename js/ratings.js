2ef7cdce09ca8988f91fcaa690a044238a95661f
rpost-intertech <62145451+rpost-intertech@users.noreply.github.com>
2020-03-13 09:56:43 -0500

    Update ratings.js

---
+++ js/ratings.js
@@ -0,0 +1,48 @@
+function collect_ratings() {
+    /*ratings object*/
+    let ratings = {
+        count: 0,
+        sum: 0,
+        average: 0
+    };
+
+    let rating = 0;
+
+    /*retrieve all input elements in the HTML that have a class of rating */
+    const elements = document.querySelectorAll('.rating');
+
+    /*cycle through the elements array with forEach loop*/
+    /*arrow function is used in forEach loop*/
+    elements.forEach( element => {
+
+        /*replace the word star with nothing and convert string to number */
+        rating = parseInt(element.id.replace('star', ''));
+
+        ratings.count += parseInt(element.value);
+    
+        ratings.sum += parseInt(element.value) * rating;
+
+        if(ratings.count !== 0) {
+            ratings.average = ratings.sum / ratings.count;
+        }
+
+    });
+
+    return ratings;
+}
+
+/**
+ * add an event listener that listens for
+ *  the change event on the document
+ *  pass an arrow function as the handler
+ */
+ document.addEventListener('change', () => {
+    const ratings = collect_ratings();
+
+    /*select the element that has an id of average*/
+    const element = document.querySelector('#average');
+
+    /*set the value property of the element to ratings.average, fixed to 2 decimal places*/
+    ratings.average = (element.value).toFixed(2);
+    
+ });
